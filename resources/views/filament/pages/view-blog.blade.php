129c4d5d6d435794d076671ba37257dbe9f1bea3
Ahmedhamdy-eng <ahmedmandoo873@gmail.com>
2024-08-06 16:02:17 +0300

    add blogs

--- resources/views/filament/pages/view-blog.blade.php
+++ resources/views/filament/pages/view-blog.blade.php
@@ -4,7 +4,7 @@
 
     @if($this->record->type == "video")
     <video controls>
-        <source src="{{ $this->record->getMedia("attachment")[0]->getFullUrl() }}" type="{{ $this->record->getMedia("default")[0]->mime_type}}">
+        <source src="{{ $this->record->getMedia("attachment")[0]->getFullUrl() }}" type="{{ $this->record->getMedia("attachment")[0]->mime_type}}">
         Your browser does not support the video tag.
     </video>
     @else
